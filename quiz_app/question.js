a951d25dca0aa41ab047246272218e05ebac1070
osterlie <josterlieadjei@gmail.com>
2020-07-20 13:28:46 +0000

    added js assignments 4 apps

---
+++ quiz_app/question.js
@@ -0,0 +1,72 @@
+let questions = [{
+	"question": "What two things did God create in the beginning (Gen. 1:1)?",
+	"option1": "The light and the darkness",
+	"option2": "The heavens and the earth",
+	"option3": "The land and the sea",
+	"option4": "The world and the stars",
+	"answer": "2"
+},{
+	"question": "What was the Spirit of God doing initially before the 7 days of creation began (Gen. 1:2)?",
+	"option1": "Breathing life on the land",
+	"option2": "Sweeping over the earth like a wind",
+	"option3": "Speaking to the waters",
+	"option4": "Hovering over the waters",
+	"answer": "4",
+},{
+	"question": "On the first day after God made the light, what two things did He separate (Gen. 1:4)?",
+	"option1": "Land and the sea",
+	"option2": "The light on the earth and the light of His presence",
+	"option3": "Light and darkness",
+	"option4": "The sun and moon",
+	"answer": "3"
+},{
+	"question": "The sequence of the day and night began on what day (Gen. 1:5)?",
+	"option1": "First",
+	"option2": "Second",
+	"option3": "Third",
+	"option4": "Fourth",
+	"answer": "1"
+},{
+	"question": "After creating the heavens and the earth, what was the first thing God created (Gen 1:3)?",
+	"option1": "Water",
+	"option2": "Land",
+	"option3": "Sky",
+	"option4": "Light",
+	"answer": "4"
+},{
+	"question": "What did God call the expanse or space He made on the second day to separate the waters above and below (Gen. 1:8)?",
+	"option1": "Sky/heaven",
+	"option2": "Oceans/seas",
+	"option3": "Outer space/stars",
+	"option4": "Clouds/Vapour",
+	"answer": "1"
+},{
+	"question": "After separating the waters above and below, what happened next to the waters below on day 3 (Gen. 1:9)? The waters were _______",
+	"option1": "Lifted up as the land rose",
+	"option2": "Frozen under the heavens",
+	"option3": "Gathered into one place",
+	"option4": "Scattered to the ends of the earth",
+	"answer": "3"
+},{
+	"question": "After the waters were fashioned into seas, what appeared on the third day (Gen 1:9)?",
+	"option1": "The birds and the fish",
+	"option2": "The mountains tops",
+	"option3": "The hand of the Lord",
+	"option4": "The dry land",
+	"answer": "4"
+},{
+	"question": "What was the land to bring forth (Gen. 1:11)?",
+	"option1": "Plants and animals",
+	"option2": "Plants and trees",
+	"option3": "Rocks and hills",
+	"option4": "Rocks and hills",
+	"answer": "2"
+},{
+	"question": "What were to mark the seasons and years (Gen 1:14)?",
+	"option1": "The ocean tides",
+	"option2": "The stars and heavens",
+	"option3": "The sun and moon",
+	"option4": "The plants and animals",
+	"answer": "3"
+},
+]
